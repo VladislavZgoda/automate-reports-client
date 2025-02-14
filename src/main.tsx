563113bc6ad99eff896cf01840f31b01d1b8c151
zgodavg <www.vlad78@yandex.ru>
2025-02-14 09:26:20 +0300

    Rename folder

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
-import Login from "./Pages/login.tsx";
+import Login from "./pages/Login.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
