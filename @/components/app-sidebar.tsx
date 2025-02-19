a8b68b66c2b8f870c5df65d6a0fee980bf94a2f8
zgodavg <www.vlad78@yandex.ru>
2025-02-19 16:49:06 +0300

    Create layout

---
+++ @/components/app-sidebar.tsx
@@ -0,0 +1,57 @@
+import { Home, Sheet, Table } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+// Menu items.
+const items = [
+  {
+    title: "Главная",
+    url: "/",
+    icon: Home,
+  },
+  {
+    title: "Экспорт Sims Client",
+    url: "#",
+    icon: Sheet,
+  },
+  {
+    title: "ОДПУ",
+    url: "#",
+    icon: Table,
+  },
+];
+
+export default function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Обработка отчётов</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,23 @@ import LoginPage from "./pages/LoginPage";
 import AuthProvider from "./providers/authProvider";
 import ProtectedRoute from "./routes/protectedRoute";
 import NotFound from "./pages/404";
+import Layout from "./layout/Layout";
 
 function App() {
   return (
     <AuthProvider>
       <Routes>
         <Route element={<ProtectedRoute />}>
-          <Route
-            path="/"
-            element={
-              <>
-                <h1 className="text-3xl font-bold underline">Hello World!</h1>
-              </>
-            }
-          />
+          <Route element={<Layout />}>
+            <Route
+              path="/"
+              element={
+                <>
+                  <h1 className="text-3xl font-bold underline">Hello World!</h1>
+                </>
+              }
+            />
+          </Route>
         </Route>
 
         <Route path="/login" element={<LoginPage />} />
