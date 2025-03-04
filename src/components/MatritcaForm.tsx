7acbc1ef5a40b64a5abc3db8b5b7fa1a6920689a
zgodavg <www.vlad78@yandex.ru>
2025-03-04 16:08:55 +0300

    Create a MatritcaForm component

---
+++ src/components/MatritcaForm.tsx
@@ -0,0 +1,102 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+const formSchema = z.object({
+  balanceGroup: z.enum(["private", "legal"], {
+    message: "Балансная группа не выбрана.",
+  }),
+  file: z
+    .instanceof(File, {
+      message: "Отсутствует xlsx файл.",
+    })
+    .refine(
+      (file) =>
+        file.type ===
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      {
+        message: "Тип файла не xlsx.",
+      },
+    ),
+});
+
+export default function MatritcaForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="balanceGroup"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-base">Балансная группа</FormLabel>
+              <FormControl>
+                <Select onValueChange={field.onChange} {...field}>
+                  <SelectTrigger className="w-[300px]">
+                    <SelectValue placeholder="Выберете балансную группу" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="private">БЫТ</SelectItem>
+                    <SelectItem value="legal">Юридические лица</SelectItem>
+                  </SelectContent>
+                </Select>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="file"
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
+          render={({ field: { value, onChange, ...fieldProps } }) => (
+            <FormItem>
+              <FormLabel className="text-base">Экспорт из Sims</FormLabel>
+              <FormControl>
+                <Input
+                  {...fieldProps}
+                  className="w-[300px]"
+                  placeholder="xlsx файл"
+                  type="file"
+                  accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                  onChange={(event) => onChange(event.target.files?.[0])}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" size="lg">
+          Сформировать
+        </Button>
+      </form>
+    </Form>
+  );
+}
--- src/pages/MatritcaExport.tsx
+++ src/pages/MatritcaExport.tsx
@@ -1,26 +1,3 @@
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-
-import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-
 import {
   Card,
   CardContent,
@@ -29,33 +6,9 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
-const formSchema = z.object({
-  balanceGroup: z.enum(["private", "legal"], {
-    message: "Балансная группа не выбрана.",
-  }),
-  file: z
-    .instanceof(File, {
-      message: "Отсутствует xlsx файл.",
-    })
-    .refine(
-      (file) =>
-        file.type ===
-        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-      {
-        message: "Тип файла не xlsx.",
-      },
-    ),
-});
+import MatritcaForm from "../components/MatritcaForm";
 
 export default function MatritcaExport() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-  });
-
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
-  }
-
   return (
     <div className="flex w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-md">
@@ -70,66 +23,7 @@ export default function MatritcaExport() {
               </CardDescription>
             </CardHeader>
             <CardContent>
-              <Form {...form}>
-                <form
-                  onSubmit={form.handleSubmit(onSubmit)}
-                  className="space-y-8"
-                >
-                  <FormField
-                    control={form.control}
-                    name="balanceGroup"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel className="text-base">
-                          Балансная группа
-                        </FormLabel>
-                        <FormControl>
-                          <Select onValueChange={field.onChange} {...field}>
-                            <SelectTrigger className="w-[300px]">
-                              <SelectValue placeholder="Выберете балансную группу" />
-                            </SelectTrigger>
-                            <SelectContent>
-                              <SelectItem value="private">БЫТ</SelectItem>
-                              <SelectItem value="legal">
-                                Юридические лица
-                              </SelectItem>
-                            </SelectContent>
-                          </Select>
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="file"
-                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                    render={({ field: { value, onChange, ...fieldProps } }) => (
-                      <FormItem>
-                        <FormLabel className="text-base">
-                          Экспорт из Sims
-                        </FormLabel>
-                        <FormControl>
-                          <Input
-                            {...fieldProps}
-                            className="w-[300px]"
-                            placeholder="xlsx файл"
-                            type="file"
-                            accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-                            onChange={(event) =>
-                              onChange(event.target.files?.[0])
-                            }
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <Button type="submit" size="lg">
-                    Сформировать
-                  </Button>
-                </form>
-              </Form>
+              <MatritcaForm />
             </CardContent>
           </Card>
         </div>
