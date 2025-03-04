21027a365453a6abe24aa0ce3fdc9a24bc4cb328
zgodavg <www.vlad78@yandex.ru>
2025-03-04 16:02:26 +0300

    Delete useless code

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -23,19 +23,17 @@ export default function LoginPage() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <div className="flex flex-col gap-6">
-          <Card>
-            <CardHeader>
-              <CardTitle className="text-2xl">Вход</CardTitle>
-              <CardDescription>
-                Введите имя учетной записи и пароль для входа.
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <LoginForm />
-            </CardContent>
-          </Card>
-        </div>
+        <Card>
+          <CardHeader>
+            <CardTitle className="text-2xl">Вход</CardTitle>
+            <CardDescription>
+              Введите имя учетной записи и пароль для входа.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <LoginForm />
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
