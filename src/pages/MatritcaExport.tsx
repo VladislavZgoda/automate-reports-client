ff8a9987cc680d4000a38715abb116349810080e
zgodavg <www.vlad78@yandex.ru>
2025-03-04 16:10:07 +0300

    Delete useless code

--- src/pages/MatritcaExport.tsx
+++ src/pages/MatritcaExport.tsx
@@ -12,21 +12,19 @@ export default function MatritcaExport() {
   return (
     <div className="flex w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-md">
-        <div className="flex flex-col gap-6">
-          <Card>
-            <CardHeader>
-              <CardTitle className="text-2xl">
-                Обработка экспорта из Sims client
-              </CardTitle>
-              <CardDescription>
-                Введите балансную группу и добавьте файл в xlsx формате.
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <MatritcaForm />
-            </CardContent>
-          </Card>
-        </div>
+        <Card>
+          <CardHeader>
+            <CardTitle className="text-2xl">
+              Обработка экспорта из Sims client
+            </CardTitle>
+            <CardDescription>
+              Введите балансную группу и добавьте файл в xlsx формате.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <MatritcaForm />
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
