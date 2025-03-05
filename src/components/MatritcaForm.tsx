3e4e63d121a843e934bd1c46907c6e8a1968c02b
Garry3Fingers <www.vlad78@yandex.ru>
2025-03-05 22:08:48 +0300

    Add a new form field

--- src/components/MatritcaForm.tsx
+++ src/components/MatritcaForm.tsx
@@ -21,27 +21,41 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 
-const formSchema = z.object({
-  balanceGroup: z.enum(["private", "legal"], {
-    message: "Балансная группа не выбрана.",
-  }),
-  file: z
-    .instanceof(File, {
-      message: "Отсутствует xlsx файл.",
-    })
-    .refine(
-      (file) =>
-        file.type ===
-        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-      {
-        message: "Тип файла не xlsx.",
-      },
-    ),
-});
+const formSchema = z
+  .object({
+    balanceGroup: z.enum(["private", "legal"], {
+      message: "Балансная группа не выбрана.",
+    }),
+    controller: z.string().optional(),
+    file: z
+      .instanceof(File, {
+        message: "Отсутствует xlsx файл.",
+      })
+      .refine(
+        (file) =>
+          file.type ===
+          "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        {
+          message: "Тип файла не xlsx.",
+        },
+      ),
+  })
+  .refine(
+    (data) =>
+      data.balanceGroup !== "private" ||
+      (data.balanceGroup === "private" && data.controller),
+    {
+      message: "Добавьте контроллера, когда выбран Быт.",
+      path: ["controller"],
+    },
+  );
 
 export default function MatritcaForm() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
+    defaultValues: {
+      controller: "",
+    },
   });
 
   function onSubmit(values: z.infer<typeof formSchema>) {
@@ -67,6 +81,23 @@ export default function MatritcaForm() {
                     <SelectItem value="legal">Юридические лица</SelectItem>
                   </SelectContent>
                 </Select>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="controller"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-base">Контроллер</FormLabel>
+              <FormControl>
+                <Input
+                  className="w-[300px]"
+                  placeholder="Введите ФИО контроллера"
+                  {...field}
+                />
               </FormControl>
               <FormMessage />
             </FormItem>
