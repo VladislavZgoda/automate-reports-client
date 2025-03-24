db5eacb282aadc7d004c6e81e495be1145462cc3
zgodavg <www.vlad78@yandex.ru>
2025-03-24 09:33:56 +0300

    Add a test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,18 +7,25 @@ import App from "./App";
 
 const flushPromises = () => new Promise((resolve) => setTimeout(resolve, 0));
 
-const server = setupServer(
+const serverResponse200 = setupServer(
   http.get("/api/refresh", () => {
     return HttpResponse.json({ accessToken: "token" }, { status: 200 });
   }),
 );
 
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
+const serverResponse401 = setupServer(
+  http.get("/api/refresh", () => {
+    return HttpResponse.json({ status: 401 });
+  }),
+);
 
-describe("App", () => {
+describe("App => Home", () => {
+  beforeAll(() => serverResponse200.listen());
+  afterEach(() => serverResponse200.resetHandlers());
+  afterAll(() => serverResponse200.close());
+
   it("renders Home component when the user is logged in", async () => {
+    
     await act(async () => {
       render(
         <BrowserRouter>
@@ -29,5 +36,24 @@ describe("App", () => {
     });
 
     expect(screen.getByText("Обработка отчётов")).toBeTruthy();
+  });
+});
+
+describe("App => Login", () => {
+  beforeAll(() => serverResponse401.listen());
+  afterEach(() => serverResponse401.resetHandlers());
+  afterAll(() => serverResponse401.close());
+
+  it("renders Login component when the user is not logged in", async () => {
+    await act(async () => {
+      render(
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>,
+      );
+      await flushPromises();
+    });
+
+    expect(screen.getByText("Войти")).toBeTruthy();
   });
 });
