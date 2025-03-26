ebf2ff4843b35d2d284eba88d99f5c48aa468625
zgodavg <www.vlad78@yandex.ru>
2025-03-26 11:39:02 +0300

    Add new tests

--- src/pages/matritcaExport/MatritcaExport.test.tsx
+++ src/pages/matritcaExport/MatritcaExport.test.tsx
@@ -1,7 +1,19 @@
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { http, HttpResponse } from "msw";
+import { setupServer } from "msw/node";
 import { BrowserRouter } from "react-router";
 import { vi } from "vitest";
+
+const server = setupServer(
+  http.get("/api/refresh", () => {
+    return HttpResponse.json({ accessToken: "token" }, { status: 200 });
+  }),
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 import MatritcaExportPage from "./MatritcaExport";
 
@@ -85,6 +97,56 @@ describe("MatritcaExportPage", () => {
       expect(
         screen.getByText("Добавьте контроллера, когда выбран Быт."),
       ).toBeTruthy();
+    });
+  });
+
+  it("shows an error if the file type is not xlsx", async () => {
+    render(<MatritcaExportPage />, { wrapper: BrowserRouter });
+
+    const fileInput = screen.getByLabelText("Экспорт из Sims");
+
+    const xlsFile = new File(["test"], "test.xls", {
+      type: "application/vnd.ms-excel",
+    });
+
+    fireEvent.change(fileInput, {
+      target: { files: { item: () => xlsFile, length: 1, 0: xlsFile } },
+    });
+
+    const submitButton = screen.getByRole("button");
+    fireEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(screen.getByText("Тип файла не xlsx.")).toBeInTheDocument();
+    });
+  });
+
+  it("does not show error for the controller field when the balance group is legal", async () => {
+    const user = userEvent.setup();
+
+    render(<MatritcaExportPage />, { wrapper: BrowserRouter });
+
+    const selectButtonElement = screen.getByRole("combobox");
+    await user.click(selectButtonElement);
+
+    const selectOption = screen.getAllByText("Юридические лица");
+    await user.click(selectOption[1]);
+
+    const fileInput = screen.getByLabelText("Экспорт из Sims");
+
+    const xlsxFile = new File(["test"], "test.xlsx", {
+      type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    });
+
+    await user.upload(fileInput, xlsxFile);
+
+    const submitButton = screen.getByRole("button");
+    fireEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(
+        screen.queryByText("Добавьте контроллера, когда выбран Быт."),
+      ).not.toBeInTheDocument();
     });
   });
 });
