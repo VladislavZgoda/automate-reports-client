f99c911d9bda18659d5f0e25b4d4cdb3ae27290a
zgodavg <www.vlad78@yandex.ru>
2025-03-27 10:56:30 +0300

    Add Card UI to component

--- src/pages/legalEntities/LegalEntities.tsx
+++ src/pages/legalEntities/LegalEntities.tsx
@@ -1,3 +1,29 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function LegalEntities() {
-  return <>Test</>;
+  return (
+    <div className="flex w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-md">
+        <Card>
+          <CardHeader>
+            <CardTitle className="text-2xl">
+              Обработка экспорта из Пирамида 2
+            </CardTitle>
+            <CardDescription>
+              Добавьте отчёт &#34;Новые показания&#34; из Пирамида 2 с данными
+              из балансных групп &#34;Юридические лица Тимашевск&#34; и &#34;ВИП
+              Тимашевск&#34;.
+            </CardDescription>
+          </CardHeader>
+          <CardContent></CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 }
