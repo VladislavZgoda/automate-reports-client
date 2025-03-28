40b24651d4afbe2a614190ee3ced5db1275ad3e9
zgodavg <www.vlad78@yandex.ru>
2025-03-28 11:04:33 +0300

    Create tests for the LoginButton component

---
+++ src/components/loginButton/LoginButton.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+
+import LoginButton from "./LoginButton";
+
+describe("FormButton component", () => {
+  it("renders the default state of the button if isSubmitting is false", () => {
+    render(<LoginButton isSubmitting={false} />);
+
+    expect(screen.getByRole("button", { name: "Войти" })).toBeInTheDocument();
+  });
+
+  it("renders the loading state of the button if isSubmitting is true", () => {
+    render(<LoginButton isSubmitting={true} />);
+
+    expect(
+      screen.getByRole("button", { name: "Проверка" }),
+    ).toBeInTheDocument();
+  });
+});
