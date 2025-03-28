8ac11a7ab7e30b7176dd6f11cd4425b44bcb6938
Garry3Fingers <www.vlad78@yandex.ru>
2025-03-28 21:40:18 +0300

    Refactor code

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,22 +5,16 @@ import { BrowserRouter } from "react-router";
 
 import App from "./App";
 
-const serverResponse200 = setupServer(
+const server = setupServer(
   http.get("/api/refresh", () => {
     return HttpResponse.json({ accessToken: "token" }, { status: 200 });
   }),
 );
 
-const serverResponse401 = setupServer(
-  http.get("/api/refresh", () => {
-    return HttpResponse.json({ status: 401 });
-  }),
-);
-
 describe("App => Home", () => {
-  beforeAll(() => serverResponse200.listen());
-  afterEach(() => serverResponse200.resetHandlers());
-  afterAll(() => serverResponse200.close());
+  beforeAll(() => server.listen());
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
 
   it("renders Home component when the user is logged in", async () => {
     render(<App />, { wrapper: BrowserRouter });
@@ -29,14 +23,14 @@ describe("App => Home", () => {
       expect(screen.getByText("Обработка отчётов")).toBeInTheDocument();
     });
   });
-});
-
-describe("App => Login", () => {
-  beforeAll(() => serverResponse401.listen());
-  afterEach(() => serverResponse401.resetHandlers());
-  afterAll(() => serverResponse401.close());
 
   it("renders Login component when the user is not logged in", async () => {
+    server.use(
+      http.get("/api/refresh", () => {
+        return HttpResponse.json("You are not authenticated.", { status: 401 });
+      }),
+    );
+
     render(<App />, { wrapper: BrowserRouter });
 
     await waitFor(() => {
