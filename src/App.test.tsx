48761734e2e526f0c1cc587d78d861fc5554cf77
Garry3Fingers <www.vlad78@yandex.ru>
2025-04-04 22:03:07 +0300

    Fix tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,34 +7,34 @@ import App from "./App";
 
 const server = setupServer(
   http.get("/api/refresh", () => {
-    return HttpResponse.json({ accessToken: "token" }, { status: 200 });
+    return HttpResponse.json("You are not authenticated.", { status: 401 });
   }),
 );
 
+ beforeAll(() => server.listen());
+ afterEach(() => server.resetHandlers());
+ afterAll(() => server.close());
+
 describe("App", () => {
-  beforeAll(() => server.listen());
-  afterEach(() => server.resetHandlers());
-  afterAll(() => server.close());
-
-  it("renders Home component when the user is logged in", async () => {
-    render(<App />, { wrapper: BrowserRouter });
-
-    const h1HomePage = await screen.findByText(/Разделы меню/);
-
-    expect(h1HomePage).toBeInTheDocument();
-  });
-
   it("renders Login component when the user is not logged in", async () => {
-    server.use(
-      http.get("/api/refresh", () => {
-        return HttpResponse.json("You are not authenticated.", { status: 401 });
-      }),
-    );
-
     render(<App />, { wrapper: BrowserRouter });
 
     const h1LoginPage = await screen.findByText(/Обработка XLSX файлов/);
 
     expect(h1LoginPage).toBeInTheDocument();
+  });
+
+  it("renders Home component when the user is logged in", async () => {
+    server.use(
+      http.get("/api/refresh", () => {
+        return HttpResponse.json({ accessToken: "token" }, { status: 200 });
+      }),
+    );
+
+    render(<App />, { wrapper: BrowserRouter });
+
+    const h1HomePage = await screen.findByText(/Разделы меню/);
+
+    expect(h1HomePage).toBeInTheDocument();
   });
 });
