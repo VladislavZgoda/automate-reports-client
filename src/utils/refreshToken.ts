15136043e9d2639adf43fcdcf688ce51af771827
Garry3Fingers <www.vlad78@yandex.ru>
2025-04-07 15:48:06 +0300

    Create a refreshToken function

---
+++ src/utils/refreshToken.ts
@@ -0,0 +1,26 @@
+import { jwtDecode } from "jwt-decode";
+import refreshTokenRequest from "../api/refersh/refreshToken";
+
+export default async function refreshToken(
+  token: string,
+  setToken: (token: string) => void,
+) {
+  const decoded = jwtDecode(token);
+
+  if (!decoded.exp) {
+    throw new Error("There is something wrong with JWT token.");
+  }
+
+  const currentTime = Math.round(Date.now() / 1000);
+  const isExpired = decoded.exp <= currentTime + 120;
+
+  if (isExpired) {
+    const newToken = await refreshTokenRequest();
+
+    setToken(newToken);
+
+    return newToken;
+  } else {
+    return token;
+  }
+}
