e3c6b819db0144429aab8f85012ee066e65d15b4
Garry3Fingers <www.vlad78@yandex.ru>
2025-04-08 16:38:22 +0300

    Create a odpyRequest function

---
+++ src/api/odpy/odpyRequest.ts
@@ -0,0 +1,42 @@
+import {
+  AuthError,
+  UnprocessablePiramidaFileError,
+  UnprocessableSimsFileError,
+} from "../../utils/customErrors";
+
+import { response422Schema } from "../../validation/response422";
+
+export default async function odpyRequest(token: string, formData: FormData) {
+  const response = await fetch("api/odpy/", {
+    method: "POST",
+    body: formData,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if ([401, 403].includes(response.status)) {
+    throw new AuthError(`${response.status} ${await response.json()}`);
+  }
+
+  if (response.status === 422) {
+    const response422 = response422Schema.safeParse(await response.json());
+
+    if (!response422.success)
+      throw new Error("Invalid server response for status code 422.");
+
+    if (response422.data.file === "simsFile") {
+      throw new UnprocessableSimsFileError(response422.data.message);
+    } else if (response422.data.file === "piramidaFile") {
+      throw new UnprocessablePiramidaFileError(response422.data.message);
+    }
+  }
+
+  if (!response.ok) {
+    throw new Error(`${response.status} ${await response.json()}`);
+  }
+
+  const blob = await response.blob();
+
+  return blob;
+}
