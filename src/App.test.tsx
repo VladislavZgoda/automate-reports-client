f83e5d6bebaa6dd04b2dfbce88d2e88ee809f770
Garry3Fingers <www.vlad78@yandex.ru>
2025-04-09 16:50:55 +0300

    Refactor code

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -8,17 +8,19 @@ import App from "./App";
 const jwtToken =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7InVzZXJOYW1lIjoidGVzdCJ9LCJpYXQiOjE1MTYyMzkwMjJ9._Et3ivEu9mS8nI3AlT6V8DuaRuzYR5s74qtSFPM7Bh4";
 
-const server = setupServer(
-  http.get("/api/refresh", () => {
-    return HttpResponse.json("You are not authenticated.", { status: 401 });
-  }),
-);
+const server = setupServer();
+
+beforeAll(() => server.listen());
+afterAll(() => server.close());
 
 describe("App", () => {
-  beforeAll(() => server.listen());
-  afterAll(() => server.close());
-
   it("renders Login component when the user is not logged in", async () => {
+    server.use(
+      http.get("/api/refresh", () => {
+        return HttpResponse.json("You are not authenticated.", { status: 401 });
+      }),
+    );
+
     render(<App />, { wrapper: BrowserRouter });
 
     const h1LoginPage = await screen.findByText(/Обработка XLSX файлов/);
