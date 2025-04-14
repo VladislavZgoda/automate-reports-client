e69ebd7bc54d6317d9ab3d4b863e4acd58498713
Garry3Fingers <www.vlad78@yandex.ru>
2025-04-14 15:13:56 +0300

    Add a new schema, rename old

--- src/validation/response422.ts
+++ src/validation/response422.ts
@@ -1,6 +1,11 @@
 import { z } from "zod";
 
-export const response422Schema = z.object({
+export const response422OdpySchema = z.object({
   file: z.enum(["simsFile", "piramidaFile"]),
+  message: z.string().min(1),
+});
+
+export const response422LegalSchema = z.object({
+  file: z.enum(["meterReadings", "currentMeterReadings"]),
   message: z.string().min(1),
 });
