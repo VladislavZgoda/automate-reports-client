638d6ab11c1f307ad7f53e5c03667425bf5163a6
Garry3Fingers <www.vlad78@yandex.ru>
2025-04-20 16:20:17 +0300

    Add a Card component

--- src/pages/vip/VIP.tsx
+++ src/pages/vip/VIP.tsx
@@ -1,3 +1,29 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function VIP() {
-  return <>VIP</>;
+  return (
+    <div className="flex w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-md">
+        <Card>
+          <CardHeader>
+            <CardTitle className="text-center text-2xl">
+              Cформировать ведомости для ВИП потребителей
+            </CardTitle>
+            <CardDescription>
+              Добавьте отчёт &#34;Новые показания&#34; из Пирамида 2 с данными
+              из балансной группы &#34;ВИП Тимашевск&#34; и файл с экспортом из
+              Sims в xlsx формате.
+            </CardDescription>
+          </CardHeader>
+          <CardContent></CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 }
