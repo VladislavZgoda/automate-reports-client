970f6605d15fc512fb586945acb6a1798af7023a
zgodavg <www.vlad78@yandex.ru>
2025-04-23 10:18:26 +0300

    Create a MicrogenerationForm component

---
+++ src/components/MicrogenerationForm.tsx
@@ -0,0 +1,158 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRef } from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router";
+import { z } from "zod";
+import matritcaRequest from "../api/matritca/matritcaRequest";
+import useAuthStore from "../hooks/useAuthStore";
+import { AuthError, UnprocessableEntityError } from "../utils/customErrors";
+import downloadFile from "../utils/downloadFile";
+import refreshToken from "../utils/refreshToken";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import FormButton from "./formButton/FormButton";
+
+import {
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import Select from "./SelectWithReset";
+
+const formSchema = z.object({
+  balanceGroup: z.enum(["private", "legal"], {
+    message: "Балансная группа не выбрана.",
+  }),
+  file: z
+    .instanceof(File, {
+      message: "Отсутствует xlsx файл.",
+    })
+    .refine(
+      (file) =>
+        file.type ===
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      {
+        message: "Тип файла не xlsx.",
+      },
+    ),
+});
+
+export default function MicrogenerationForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const isSubmitting = form.formState.isSubmitting;
+
+  const accessToken = useAuthStore((state) => state.accessToken);
+  const setAccessToken = useAuthStore((state) => state.setAccessToken);
+  const resetToken = useAuthStore((state) => state.reset);
+
+  const formRef = useRef<HTMLFormElement>(null);
+  const navigate = useNavigate();
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    const formData = new FormData();
+
+    formData.append("balanceGroup", values.balanceGroup);
+    formData.append("upload", values.file);
+
+    try {
+      const token = await refreshToken(accessToken, setAccessToken);
+
+      const blob = await matritcaRequest(
+        "api/microgeneration/",
+        token,
+        formData,
+      );
+
+      downloadFile(blob, "Микрогенерация.xlsx");
+
+      formRef.current?.reset();
+      form.reset();
+    } catch (error) {
+      if (error instanceof AuthError) {
+        resetToken();
+
+        await navigate("/login");
+      } else if (error instanceof UnprocessableEntityError) {
+        form.setError("file", {
+          message:
+            "Заголовки таблицы xlsx не совпадают с заголовками файла с импортом - экспортом из Sims.",
+        });
+      } else {
+        console.error(error);
+      }
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8"
+        ref={formRef}
+      >
+        <FormField
+          control={form.control}
+          name="balanceGroup"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-base" htmlFor="balanceGroup">
+                Балансная группа
+              </FormLabel>
+              <FormControl>
+                <Select {...field} value={field.value}>
+                  <SelectTrigger
+                    className="mt-0.5 w-[300px] cursor-pointer"
+                    id="balanceGroup"
+                  >
+                    <SelectValue placeholder="Выберете балансную группу" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="private">БЫТ</SelectItem>
+                    <SelectItem value="legal">Юридические лица</SelectItem>
+                  </SelectContent>
+                </Select>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="file"
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
+          render={({ field: { value, onChange, ...fieldProps } }) => (
+            <FormItem>
+              <FormLabel className="text-base">Экспорт из Sims</FormLabel>
+              <FormControl>
+                <Input
+                  {...fieldProps}
+                  className="mt-0.5 w-[300px] cursor-pointer"
+                  placeholder="xlsx файл"
+                  type="file"
+                  accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                  onChange={(event) => onChange(event.target.files?.[0])}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormButton isSubmitting={isSubmitting} />
+      </form>
+    </Form>
+  );
+}
