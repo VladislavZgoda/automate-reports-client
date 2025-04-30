0eb2fc9b2e4c5f60237d858977bc50f49cba008c
Garry3Fingers <www.vlad78@yandex.ru>
2025-04-30 16:09:28 +0300

    Update tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,44 +1,38 @@
 import { render, screen } from "@testing-library/react";
-import { http, HttpResponse } from "msw";
-import { setupServer } from "msw/node";
 import { BrowserRouter } from "react-router";
+import { vi } from "vitest";
+import authTokenStore from "./store/authTokenStore";
 
 import App from "./App";
 
-const jwtToken =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7InVzZXJOYW1lIjoidGVzdCJ9LCJpYXQiOjE1MTYyMzkwMjJ9._Et3ivEu9mS8nI3AlT6V8DuaRuzYR5s74qtSFPM7Bh4";
-
-const server = setupServer();
-
-beforeAll(() => server.listen());
-afterAll(() => server.close());
+vi.mock(import("jwt-decode"), async (importOriginal) => {
+  const actual = await importOriginal();
+  return {
+    ...actual,
+    jwtDecode: () => {
+      return "";
+    },
+  };
+});
 
 describe("App", () => {
-  it("renders Login component when the user is not logged in", async () => {
-    server.use(
-      http.get("/api/refresh", () => {
-        return HttpResponse.json("You are not authenticated.", { status: 401 });
-      }),
-    );
-
-    render(<App />, { wrapper: BrowserRouter });
-
-    const h1LoginPage = await screen.findByText(/Обработка XLSX файлов/);
-
-    expect(h1LoginPage).toBeInTheDocument();
-  });
-
   it("renders Home component when the user is logged in", async () => {
-    server.use(
-      http.get("/api/refresh", () => {
-        return HttpResponse.json({ accessToken: jwtToken }, { status: 200 });
-      }),
-    );
+    authTokenStore.getState().setAccessToken("token");
 
     render(<App />, { wrapper: BrowserRouter });
 
     const h1HomePage = await screen.findByText(/Разделы меню/);
 
     expect(h1HomePage).toBeInTheDocument();
+
+    authTokenStore.getState().reset();
+  });
+
+  it("renders Login component when the user is not logged in", async () => {
+    render(<App />, { wrapper: BrowserRouter });
+
+    const h1LoginPage = await screen.findByText(/Обработка XLSX файлов/);
+
+    expect(h1LoginPage).toBeInTheDocument();
   });
 });
