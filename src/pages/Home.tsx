8627c8ee05bd7d8633237c976328455dc8e7cb73
Garry3Fingers <www.vlad78@yandex.ru>
2025-09-06 16:31:55 +0300

    Refactor Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,81 +1,83 @@
-import { Dot, Menu } from "lucide-react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 export default function Home() {
   return (
-    <div className="mt-0.5 ml-1.5">
-      <div className="ml-[-4px] flex items-center gap-1">
-        <Menu size="20px" className="mb-1.5" />
-        <h1 className="mb-3 text-2xl font-bold underline">Разделы меню:</h1>
-      </div>
-
-      <div className="mb-0.5 ml-[-7px] flex items-center gap-1">
-        <Dot strokeWidth="3px" />
-        <h2 className="text-lg font-bold">Экспорт Sims Client</h2>
-      </div>
-      <p className="ml-5.5 max-w-3xl text-pretty">
-        Позволяет преобразовать экспорт из ПО Sims, в формате xlsx, в
-        &#34;Приложение №9 БЫТ/Юридические лица&#34; и &#34;Ведомость
-        дистанционного снятия показаний посредствам АСКУЭ&#34;.
-      </p>
-
-      <div className="mt-3 mb-0.5 ml-[-7px] flex items-center gap-1">
-        <Dot strokeWidth="3px" />
-        <h2 className="text-lg font-bold">ОДПУ</h2>
-      </div>
-      <p className="ml-5.5 max-w-3xl text-pretty">
-        Позволяет преобразовать экспорт из ПО Sims и &#34;Отчёт по показаниям,
-        по тарифам (Сут А+)&#34; из ПО Пирамида 2, в формате xlsx, в
-        &#34;Приложение №9 ОДПУ&#34; и &#34;Ведомость дистанционного снятия
-        показаний посредствам АСКУЭ&#34;.
-      </p>
-
-      <div className="mt-3 mb-0.5 ml-[-7px] flex items-center gap-1">
-        <Dot strokeWidth="3px" />
-        <h2 className="text-lg font-bold">Юридические лица П2</h2>
-      </div>
-      <p className="ml-5.5 max-w-3xl text-pretty">
-        Позволяет преобразовать экспорт из ПО Пирамида 2, в формате xlsx, в
-        &#34;Приложение №9 Юридические лица&#34; и не большие ведомости для
-        отправки конкретным потребителями по электронной почте.
-      </p>
-
-      <div className="mt-3 mb-0.5 ml-[-7px] flex items-center gap-1">
-        <Dot strokeWidth="3px" />
-        <h2 className="text-lg font-bold">ВИП</h2>
-      </div>
-      <p className="ml-5.5 max-w-3xl text-pretty">
-        Позволяет преобразовать экспорт из ПО Пирамида 2 и Sims, в формате xlsx,
-        в ведомости для отправки на ящики филиалов по электронной почте.
-      </p>
-
-      <div className="mt-3 mb-0.5 ml-[-7px] flex items-center gap-1">
-        <Dot strokeWidth="3px" />
-        <h2 className="text-lg font-bold">Микрогенерация</h2>
-      </div>
-      <p className="ml-5.5 max-w-3xl text-pretty">
-        Позволяет преобразовать экспорт из ПО Sims, в формате xlsx, в ведомость
-        с Микрогенерацией.
-      </p>
-
-      <div className="mt-3 mb-0.5 ml-[-7px] flex items-center gap-1">
-        <Dot strokeWidth="3px" />
-        <h2 className="text-lg font-bold">Не загруженные 25</h2>
-      </div>
-      <p className="ml-5.5 max-w-3xl text-pretty">
-        Позволяет преобразовать экспорт из ПО Sims, в формате xlsx, в файл из
-        которого удаленны бытовые потребители, показания которых были переданы
-        25 числа.
-      </p>
-
-      <div className="mt-3 mb-0.5 ml-[-7px] flex items-center gap-1">
-        <Dot strokeWidth="3px" />
-        <h2 className="text-lg font-bold">Изменить однозонные NP7x</h2>
-      </div>
-      <p className="ml-5.5 max-w-3xl text-pretty">
-        Позволяет изменить Т1 и Т2 однозонных счетчиков NP7x из экспорта ПО
-        Sims, в формате xlsx. Для этого нужно нужен файл с серийными номерами
-        записаными в столбец &#34;A&#34;.
-      </p>
-    </div>
+    <Accordion
+      type="single"
+      collapsible
+      className="mt-0.5 ml-1.5 w-full"
+      defaultValue="item-1"
+    >
+      <AccordionItem value="item-1">
+        <AccordionTrigger className="text-lg font-bold">
+          Экспорт Sims Client
+        </AccordionTrigger>
+        <AccordionContent className="max-w-2xl text-lg text-pretty">
+          Позволяет преобразовать экспорт из ПО Sims, в формате xlsx, в
+          &#34;Приложение №9 БЫТ/Юридические лица&#34; и &#34;Ведомость
+          дистанционного снятия показаний посредствам АСКУЭ&#34;.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-2">
+        <AccordionTrigger className="text-lg font-bold">ОДПУ</AccordionTrigger>
+        <AccordionContent className="max-w-2xl text-lg text-pretty">
+          Позволяет преобразовать экспорт из ПО Sims и &#34;Отчёт по показаниям,
+          по тарифам (Сут А+)&#34; из ПО Пирамида 2, в формате xlsx, в
+          &#34;Приложение №9 ОДПУ&#34; и &#34;Ведомость дистанционного снятия
+          показаний посредствам АСКУЭ&#34;.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-3">
+        <AccordionTrigger className="text-lg font-bold">
+          Юридические лица П2
+        </AccordionTrigger>
+        <AccordionContent className="max-w-2xl text-lg text-pretty">
+          Позволяет преобразовать экспорт из ПО Пирамида 2, в формате xlsx, в
+          &#34;Приложение №9 Юридические лица&#34; и не большие ведомости для
+          отправки конкретным потребителями по электронной почте.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-4">
+        <AccordionTrigger className="text-lg font-bold">ВИП</AccordionTrigger>
+        <AccordionContent className="max-w-2xl text-lg text-pretty">
+          Позволяет преобразовать экспорт из ПО Пирамида 2 и Sims, в формате
+          xlsx, в ведомости для отправки на ящики филиалов по электронной почте.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-5">
+        <AccordionTrigger className="text-lg font-bold">
+          Микрогенерация
+        </AccordionTrigger>
+        <AccordionContent className="max-w-2xl text-lg text-pretty">
+          Позволяет преобразовать экспорт из ПО Sims, в формате xlsx, в
+          ведомость с Микрогенерацией.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-6">
+        <AccordionTrigger className="text-lg font-bold">
+          Не загруженные 25
+        </AccordionTrigger>
+        <AccordionContent className="max-w-2xl text-lg text-pretty">
+          Позволяет преобразовать экспорт из ПО Sims, в формате xlsx, в файл из
+          которого удаленны бытовые потребители, показания которых были переданы
+          25 числа.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-7">
+        <AccordionTrigger className="text-lg font-bold">
+          Изменить однозонные NP7x
+        </AccordionTrigger>
+        <AccordionContent className="max-w-2xl text-lg text-pretty">
+          Позволяет изменить Т1 и Т2 однозонных счетчиков NP7x из экспорта ПО
+          Sims, в формате xlsx. Для этого нужно нужен файл с серийными номерами
+          записаными в столбец &#34;A&#34;.
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
   );
 }
